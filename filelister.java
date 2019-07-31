e550cca649db10b09ff41d36a233fa31059a55cb
Matthew Anderson <44532345+qztv5@users.noreply.github.com>
2019-07-31 19:54:25 -0400

    Initial commit

---
+++ filelister.java
@@ -0,0 +1,20 @@
+package com.Anderson.homework3;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class filelister {
+
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a directory where you would like so see all files and dircectories");
+		File folder = new File(in.next());
+		String[] files = folder.list();
+		
+		for(String file: files)
+		{
+			System.out.println(file);
+		}
+	}
+}
